ad385acfb2640d650eedf002aee96a130cb6d6d7
Stefan Haeling <stefanhaeling95@gmail.com>
2022-06-10 18:01:33 +0200

    Created ERMs Created admin view and edit of models

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from .models import User
+from .models import GolfClub
+from .models import GolfCourse
+from .models import Hole
+from .models import UserHoleScore
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        '_firstName',
+        '_lastName',
+        '_handicap',
+        '_phoneNumber'
+    )
+
+
+class GolfClubAdmin(admin.ModelAdmin):
+    list_display = (
+        '_name',
+        '_streetHouseNumber',
+        '_zip',
+        '_city',
+        '_website',
+        '_email',
+        '_phoneNumber'
+    )
+
+
+class GolfCourseAdmin(admin.ModelAdmin):
+    list_display = (
+        '_golfClub',
+        '_name',
+        '_holesAmount',
+    )
+
+
+class HoleAdmin(admin.ModelAdmin):
+    list_display = (
+        '_golfCourse',
+        '_courseNameIdentifier',
+        '_holeNumber',
+        '_par',
+        '_distance',
+        '_hcp'
+    )
+
+
+class UserHoleScoreAdmin(admin.ModelAdmin):
+    list_display = (
+        '_user',
+        '_hole',
+        '_score'
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(GolfClub, GolfClubAdmin)
+admin.site.register(GolfCourse, GolfCourseAdmin)
+admin.site.register(Hole, HoleAdmin)
+admin.site.register(UserHoleScore, UserHoleScoreAdmin)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    _firstName = models.CharField(max_length=48)
+    _lastName = models.CharField(max_length=48)
+    _handicap = models.IntegerField()
+    _phoneNumber = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self._firstName + ' ' + self._lastName
+
+
+class GolfClub(models.Model):
+    _name = models.CharField(max_length=128)
+    _streetHouseNumber = models.CharField(max_length=128)
+    _zip = models.CharField(max_length=24)
+    _city = models.CharField(max_length=128)
+    _website = models.CharField(max_length=64)
+    _email = models.CharField(max_length=128)
+    _phoneNumber = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self._name
+
+
+class GolfCourse(models.Model):
+    _golfClub = models.ForeignKey(GolfClub, on_delete=models.CASCADE)
+    _name = models.CharField(max_length=128)
+    _holesAmount = models.IntegerField()
+
+    def __str__(self):
+        return self._name
+
+
+class Hole(models.Model):
+    _golfCourse = models.ForeignKey(GolfCourse, on_delete=models.CASCADE)
+    _courseNameIdentifier = models.CharField(max_length=128)
+    _holeNumber = models.IntegerField()
+    _par = models.IntegerField()
+    _distance = models.IntegerField()
+    _hcp = models.IntegerField()
+
+    def __str__(self):
+        if self._courseNameIdentifier != '' or self._courseNameIdentifier:
+            return 'Course:' + str(self._courseNameIdentifier) + ', Hole: ' + str(self._holeNumber)
+        else:
+            return str(self._holeNumber)
+
+
+class UserHoleScore(models.Model):
+    _user = models.ForeignKey(User, on_delete=models.CASCADE)
+    _hole = models.ForeignKey(Hole, on_delete=models.CASCADE)
+    _score = models.IntegerField()
+
+    def __int__(self):
+        return self._score
